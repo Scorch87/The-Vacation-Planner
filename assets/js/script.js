05e7017a6d53d419591de7687c678f05dbba9b4e
Scorch87 <44007402+Scorch87@users.noreply.github.com>
2023-01-26 19:26:42 -0600

    updated script.js for map functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-//Relocated script.js to ./assets/js/
+// new platform object with out API key.
+// in order for this to work, I had to start a server service on my 
+// machine to make the api calls from in order to get around the
+// CORS issue. E. Pruneda 1/26/23
+// python -m http.server 8888  <-- server set up on bash cli
+
+var platform = new H.service.Platform({
+    'apikey': 'SEUg7I2NeQOyvkWCqRxjhcdBI3-h3f6PV81fdKrEwMg'
+});
+  
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 7,
+      center: { lat: 39.0997, lng: -94.5786 }
+    });
