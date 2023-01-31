72b74202c5b62a5dda3f113d54f2102796f6f8f0
Scorch87 <44007402+Scorch87@users.noreply.github.com>
2023-01-30 20:41:03 -0600

    script.js reworked for Google Maps API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,111 @@
-// new platform object with out API key.
-// in order for this to work, I had to start a server service on my 
-// machine to make the api calls from in order to get around the
-// CORS issue. E. Pruneda 1/26/23
-// python -m http.server 8888  <-- server set up on bash cli
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let map, infoWindow;
+let marker, geocoder, response;
 
-var platform = new H.service.Platform({
-    'apikey': 'SEUg7I2NeQOyvkWCqRxjhcdBI3-h3f6PV81fdKrEwMg'
-});
+// Simple Map
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 39.099724, lng: -94.578331 },
+    zoom: 8,
+  });
+
+// infoWindow is geolocating to the user's current location
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+//   Pan to current location button
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+//   Search button functionality
+    geocoder = new google.maps.Geocoder();
+
+    const inputText = document.createElement("input");
+
+    inputText.type = "text";
+    inputText.placeholder = "Enter a location";
+
+    const submitButton = document.createElement("input");
+
+    submitButton.type = "button";
+    submitButton.value = "Take me there";
+    submitButton.classList.add("button", "button-primary");
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
+
+    //set a marker @ location
+    marker = new google.maps.Marker({
+        map,
+      });
+      map.addListener("click", (e) => {
+        geocode({ location: e.latLng });
+      });
+      submitButton.addEventListener("click", () =>
+        geocode({ address: inputText.value })
+      );
+}
+// error catching
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function clear() {
+    marker.setMap(null);
+    // responseDiv.style.display = "none";
+}
+
+// Geocoding function
+function geocode(request) {
+    clear();
+    geocoder
+      .geocode(request)
+      .then((result) => {
+        const { results } = result;
   
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-      zoom: 7,
-      center: { lat: 39.0997, lng: -94.5786 }
-    });
-
-    // MapEvents object from the HERE library
-var mapEvents = new H.mapevents.MapEvents(map);
-
-map.addEventListener('tap', function(evt) {
-    // Log 'tap' and 'mouse' events:
-    console.log(evt.type, evt.currentPointer.type); 
-});
-
-// Instantiate the default behavior, providing the mapEvents object:
-var behavior = new H.mapevents.Behavior(mapEvents);
+        map.setCenter(results[0].geometry.location);
+        marker.setPosition(results[0].geometry.location);
+        marker.setMap(map);
+        // responseDiv.style.display = "block";
+        // response.innerText = JSON.stringify(result, null, 2);
+        return results;
+      })
+      .catch((e) => {
+        alert("Geocode was not successful for the following reason: " + e);
+      });
+  }
+//Initialize the map
+window.initMap = initMap;
